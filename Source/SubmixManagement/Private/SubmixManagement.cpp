595a79700e477c7befdc0fabc06879aa9395d639
BBavoso <bmbavoso@gmail.com>
2026-04-05 16:41:27 -0400

    add submix managment module

---
+++ Source/SubmixManagement/Private/SubmixManagement.cpp
@@ -0,0 +1,17 @@
+﻿#include "SubmixManagement.h"
+
+#define LOCTEXT_NAMESPACE "FSubmixManagementModule"
+
+void FSubmixManagementModule::StartupModule()
+{
+    
+}
+
+void FSubmixManagementModule::ShutdownModule()
+{
+    
+}
+
+#undef LOCTEXT_NAMESPACE
+    
+IMPLEMENT_MODULE(FSubmixManagementModule, SubmixManagement)
